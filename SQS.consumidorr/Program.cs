0a7d4c108493ab4d09896592139cb918278fb5da
Eduardo Gabriel de Freitas <58999344+Tironsom@users.noreply.github.com>
2024-04-25 21:40:06 -0300

    Adicionar arquivos de projeto.

---
+++ SQS.consumidorr/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using Amazon.Runtime;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using System.Collections;
+using Amazon.SQS;
+using Amazon;
+
+Console.WriteLine("Hello, World!"); 
+var client = new AmazonSQSClient(RegionEndpoint.SAEast1);
+var request = new SendMessageRequest
+{
+    QueueUrl = "https://sqs.sa-east-1.amazonaws.com/767398107071/filinha",
+    MessageBody = "teste 123"
+};
+
+await client.SendMessageAsync( request );
---
+++ sqs.fornecedorr/Program.cs
@@ -0,0 +1,21 @@
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        var client = new AmazonSQSClient(RegionEndpoint.SAEast1);
+        var request = new ReceiveMessageRequest
+        {
+            QueueUrl = "https://sqs.sa-east-1.amazonaws.com/767398107071/filinha"
+        };
+        var response = await client.ReceiveMessageAsync(request);
+    
+    }
+}
